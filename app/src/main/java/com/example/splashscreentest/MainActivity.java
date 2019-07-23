0c75daf9e04aa0ed4560865017582a26c90d915f
Kaleb Kougl <kalebkougl@gmail.com>
2019-07-23 15:18:44 -0500

    adding internet permission, adding volley to make http requests, adding simple request to test internet connection

--- app/src/main/java/com/example/splashscreentest/MainActivity.java
+++ app/src/main/java/com/example/splashscreentest/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+
+//    for testing purposes
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +35,32 @@ public class MainActivity extends AppCompatActivity {
 //                Intent numberIntent = new Intent(MainActivity.this,
 //                        NumbersActivity.class);
 //                startActivity(numberIntent);
-                Toast toast = Toast.makeText(MainActivity.this, "You clicked on the + button", Toast.LENGTH_SHORT);
-                toast.setMargin(50, 50);
-                toast.show();
+
+                final TextView textView = (TextView) findViewById(R.id.first_text_view);
+                final Toast toast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT);
+                textView.setText("working on the http request meow");
+
+// Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                textView.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "onErrorResponse: BAD HTTP REQUEST" + error.toString(), error);
+                        textView.setText("That didn't work!");
+                    }
+                });
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
     }
