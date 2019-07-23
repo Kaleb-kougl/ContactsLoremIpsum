b56897710bae77ef2abd0f4cf74563a3a785448f
Kaleb Kougl <kalebkougl@gmail.com>
2019-07-23 13:57:41 -0500

    adding an event listener that is in line, adding a test toast to make sure that the event listener is working correctly

--- app/src/main/java/com/example/splashscreentest/MainActivity.java
+++ app/src/main/java/com/example/splashscreentest/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.splashscreentest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Event listener for the add button
+        TextView add = (TextView) findViewById(R.id.add_button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent numberIntent = new Intent(MainActivity.this,
+//                        NumbersActivity.class);
+//                startActivity(numberIntent);
+                Toast toast = Toast.makeText(MainActivity.this, "You clicked on the + button", Toast.LENGTH_SHORT);
+                toast.setMargin(50, 50);
+                toast.show();
+            }
+        });
     }
 }
