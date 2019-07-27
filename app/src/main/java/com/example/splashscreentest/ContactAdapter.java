6a8dfb785b1a74fbea6bc90fd40fd8ddb4dafabb
Kaleb Kougl <kalebkougl@gmail.com>
2019-07-27 12:40:07 -0500

    fixing what android studio broke, I think the array adapter is now fixed and will show images

--- app/src/main/java/com/example/splashscreentest/ContactAdapter.java
+++ app/src/main/java/com/example/splashscreentest/ContactAdapter.java
@@ -1,12 +1,19 @@
 package com.example.splashscreentest;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
@@ -26,7 +33,7 @@ public class ContactAdapter extends ArrayAdapter {
 
 //        TODO: Make an array of contacts in the main activity
         Contact currentContact = (Contact) getItem(position);
-        ImageView contactImage = (ImageView) contactView.findViewById(
+        final ImageView contactImage = (ImageView) contactView.findViewById(
                 R.id.contact_row_image_view);
         TextView contactFirstName = (TextView) contactView.findViewById(
                 R.id.contact_row_first_name_text_view);
@@ -36,6 +43,32 @@ public class ContactAdapter extends ArrayAdapter {
 //        contactImage.setImageResource(...)
         contactFirstName.setText(currentContact.getFirstName());
         contactLastName.setText(currentContact.getLastName());
+        if (currentContact.getIsMale()) {
+            contactImage.setImageResource(R.drawable.male);
+        } else {
+            contactImage.setImageResource(R.drawable.female);
+        }
+
+        /**
+         * This will make an image request via volley
+         * Then it will add it to the request queue
+         * On success it will add the image to the ImageView
+         * On fail it does NOTHING.
+         */
+        ImageRequest request = new ImageRequest(currentContact.getPictureUrlString(),
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        contactImage.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(request);
         return contactView;
     }
 }
